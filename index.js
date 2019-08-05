6b70c72e0d54b4b4fbd0c930a5bcb813712ec180
Vishnu <vvichugit@gmail.com>
2019-08-04 19:51:04 -0500

    added the add Function

--- index.js
+++ index.js
@@ -17,10 +17,45 @@ class BST{
     }
 
     addData(data){
-      this.root = new Node(data); 
+        if(this.root === null){
+          this.root = new Node(data);
+        }else{
+            const root = this.root;
+            const searchTerm = function(root){
+            if(data > root.data){
+                if(root.right === null){
+                  root.right = new Node(data);
+                  return;
+                }else{
+                    return searchTerm(root.right);
+                }
+
+            }else if(data < root.data){
+                if(root.left === null){
+                  root.left = new Node(data);
+                  return; 
+                }else{
+                   return searchTerm(root.left);
+                }
+            }else{
+                return;
+            }
+
+        }
+          return searchTerm(root);
+        }
     }
+
+
 }
 
 const b2 = new BST; 
 b2.addData(100);
+b2.addData(75);
+b2.addData(150);
+b2.addData(80);
+b2.addData(60);
+b2.addData(200);
+b2.addData(140);
+b2.addData(30);
 console.log(b2);
